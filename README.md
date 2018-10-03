516504f71879968baff3d6d52a2f37ed891224bd
Amanda <anikkole796@gmail.com>
2018-10-02 21:02:15 -0700

    found most popular type per year

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Avocados
 
-This set of experiments was inspired by the dataset found on Kaggle (https://www.kaggle.com/neuromusic/avocado-prices/home).
+This set of experiments was inspired by the dataset found on [Kaggle](https://www.kaggle.com/neuromusic/avocado-prices/home). It was uploaded around June 2018.
 
 A lot of people make jokes about how much millenials __love__ avocados, so I wanted to quantitatively measure that love and see how machine learning can provide additional insight into avocado consumption of today.
 
+## What this repo aims to do
 Curious about how consumption of avocados have changed throughout the years, I have parsed the dataset to find simple statistics.
+Run `python avocado_stats.py` to :
+- Find the average price of an avocado per year and see the changes in a plotted graph
+- Find the most popular type of avocado per year and how much of that type was sold 
 
 In addition to these statistics, I'd like to know how well the year the avocado was produced can be predicted through its price and location.
 
 In the works: building a classifier to give results to the above statement.
   
+** Original source of data: [Avocado Board](http://www.hassavocadoboard.com/retail/volume-and-price-data)
--- avocado_stats.py
+++ avocado_stats.py
@@ -1,63 +1,108 @@
-import pandas as pd
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from scipy.stats import scoreatpercentile
 import csv
 import matplotlib.pyplot as plt
-""
+
 
 # read from input data file
 with open('data/avocado.csv') as file:
     data = list(csv.reader(file, delimiter=','))
 
-print('%d total data points' % len(data))
+print('There are %d total data points' % (len(data)-1))
+
 # create np array without column titles
 data = np.array(data[1:])
+
+# find the average price of avocados and total amount sold throughout all the years
 years = data[:, 12]
+distinct_years = set(years)
 avg_prices = data[:, 2].astype(float)
 total_bags = data[:, 8].astype(float)
-distinct_years = set(years)
-print(distinct_years)
-print(avg_prices)
-print(total_bags)
-
 overall_avg_price = np.mean(avg_prices)
 overall_total_bags = np.sum(total_bags)
-print('The average price of avocados from 2015 to 2018 is $%.2f' % overall_avg_price)
-print('The total number of bags of avocados sold from 2015 to 2018 is %d' % overall_total_bags)
+print('The average price of avocados from %s to %s is $%.2f' % (min(distinct_years), max(distinct_years), overall_avg_price))
+print('The total number of bags of avocados sold in this period is %d' % overall_total_bags)
 
-years_to_prices = {}
-for year in distinct_years:
-    years_to_prices[year] = []
 
 """
+---------------------------------------------------------------------------
 To find out how the average price of avocados has changed since 2015,
 find out how the average price per year compare to each other
+---------------------------------------------------------------------------
 """
-for row in data:
-    year = row[12]
-    price = row[2].astype(float)
-    # print(price)
-    # np.append(years_to_prices[year], np.array(price))
-    years_to_prices[year].append(price)
-
-plot_values, x_values, y_values = [], [], []
-for year in sorted(years_to_prices.keys()):
-    years_to_prices[year] = np.array(years_to_prices[year])
-    average_price_per_year = np.mean(years_to_prices[year])
-    print(year, average_price_per_year)
-    y_values.append(average_price_per_year)
 
 
-plt.plot(sorted(distinct_years), y_values)
-plt.title("Average price of avocados per year")
-plt.xlabel("Year")
-plt.ylabel("Price")
-plt.show()
+def price_per_year(data):
+    # initialize dictionary that map years to prices
+    years_to_prices = {}
+    for year in distinct_years:
+        years_to_prices[year] = []
+
+    for row in data:
+        year = row[12]
+        price = row[2].astype(float)
+        years_to_prices[year].append(price)
+
+    y_values = []
+    print('Year\tAverage Price')
+    for year in sorted(years_to_prices.keys()):
+        years_to_prices[year] = np.array(years_to_prices[year])
+        average_price_per_year = np.mean(years_to_prices[year])
+        print(year, average_price_per_year)
+        y_values.append(average_price_per_year)
 
 
+    plt.plot(sorted(distinct_years), y_values)
+    plt.title("Average price of avocados per year")
+    plt.xlabel("Year")
+    plt.ylabel("Price")
+    plt.show()
 
 
+"""
+---------------------------------------------------------------------------
+To find out the most popular kind of avocado (based on plu code) per year,
+find the max average total of each avocado type for each year
+
+Column titles that we care about: '4046', '4225', '4770'
+---------------------------------------------------------------------------
+"""
 
 
+def most_popular_per_year(data):
+    codes = ['4046', '4225', '4770']
+    plu_lists_by_year, year_to_type_amount = {}, {}
+
+    # initialize dictionary plu_lists_by_year with a list of lists (all values per type of avocado per year)
+    for year in distinct_years:
+        year_to_type_amount[year] = ()
+        plu_lists_by_year[year] = [[], [], []]
+
+    for row in data:
+        year = row[12]
+        plu_4046 = row[4].astype(float)
+        plu_4225 = row[5].astype(float)
+        plu_4770 = row[6].astype(float)
+        plu_lists_by_year[year][0].append(plu_4046)
+        plu_lists_by_year[year][1].append(plu_4225)
+        plu_lists_by_year[year][2].append(plu_4770)
+
+    # determine which type has the highest total; write to file the plu_code and value of the highest total per year
+    for year in plu_lists_by_year.keys():
+        max_total = 0.0
+        for index, plu_values in enumerate(plu_lists_by_year[year]):
+            total = np.sum(np.array(plu_values))
+            if total > max_total:
+                max_total = total
+                year_to_type_amount[year] = (codes[index], total)
+
+    print(sorted(year_to_type_amount.items()))
+    with open('results/most_popular_type_per_year','w+') as out_file:
+        out_file.write('Year |\tPLU  |\tTotal Sold\n')
+        out_file.write('--------------------------\n')
+        for year, type_tuple in sorted(year_to_type_amount.items()):
+            output_string = '%s |\t%s |\t%d\n' % (year, type_tuple[0], type_tuple[1])
+            out_file.write(output_string)
+
+
+price_per_year(data)
+most_popular_per_year(data)
